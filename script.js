0170867c2b261731053de7b7ea7589c491ffb5bc
mrndolo <callistomutunga@gmail.com>
2024-07-07 18:25:59 +0300

    Displayed the days and months in words

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const year = document.getElementById("year");
 const today = new Date();
 // console.log(today);
 
-date.innerHTML = today.getDate();
-day.innerHTML = today.getDay();
-month.innerHTML = today.getMonth();
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+date.innerHTML = (today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
 year.innerHTML = today.getFullYear();
 
